7fad8d1eac5b09082a711eaa8d8116081bba6de2
Narendra Mahara <narendramahara85131@gmail.com>
2024-08-19 22:51:45 +0545

    background changes with mode

--- toggleButton/script.js
+++ toggleButton/script.js
@@ -1,13 +1,16 @@
 let light = document.getElementsByClassName("light")[0];
 let dark = document.getElementsByClassName("dark")[0];
 let hidden = document.getElementsByClassName("hidden")[0];
+let body = document.querySelector("body");
 
 light.addEventListener("click", () => {
   light.classList.add("hidden");
   dark.classList.remove("hidden");
+  body.style.backgroundColor = "black";
 });
 
 dark.addEventListener("click", () => {
   dark.classList.add("hidden");
   light.classList.remove("hidden");
+  body.style.backgroundColor = "white";
 });
