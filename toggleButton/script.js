744d3f01bac3fe009b53990f834df9b858212235
Narendra Mahara <narendramahara85131@gmail.com>
2024-08-20 15:23:55 +0545

    final commit

--- toggleButton/script.js
+++ toggleButton/script.js
@@ -1,16 +1,25 @@
 let light = document.getElementsByClassName("light")[0];
 let dark = document.getElementsByClassName("dark")[0];
-let hidden = document.getElementsByClassName("hidden")[0];
 let body = document.querySelector("body");
+let circle = document.querySelector(".circle");
+let main = document.querySelector(".main");
+body.style.transition = "background-color 0.6s";
+main.style.transition = "box-shadow 0.6s";
 
 light.addEventListener("click", () => {
-  light.classList.add("hidden");
-  dark.classList.remove("hidden");
+  circle.style.transition = " all 0.6s";
   body.style.backgroundColor = "black";
+  circle.style.transform = "translateX(88px)";
+  circle.style.backgroundColor = "black";
+  circle.style.boxShadow = "2px 2px 5px black";
+  main.style.boxShadow = "2px 2px 5px white, -2px -2px 5px white";
 });
 
 dark.addEventListener("click", () => {
-  dark.classList.add("hidden");
-  light.classList.remove("hidden");
+  circle.style.transition = " all 0.6s";
   body.style.backgroundColor = "white";
+  circle.style.transform = "translateX(0px)";
+  circle.style.backgroundColor = "white";
+  circle.style.boxShadow = "2px 2px 5px white";
+  main.style.boxShadow = "2px 2px 5px black, -2px -2px 5px black";
 });
